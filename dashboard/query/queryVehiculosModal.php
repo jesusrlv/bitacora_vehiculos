9a9fd7c9a6ecd57958b00b6c32a2b31370029002
jesusrlv <jesusrlvrojo@gmail.com>
2026-03-10 11:03:16 -0600

    acomodo vehiculos

--- dashboard/query/queryVehiculosModal.php
+++ dashboard/query/queryVehiculosModal.php
@@ -6,12 +6,15 @@
     $x = 0;
     while($row = $resultadoSql->fetch_assoc()) {
         $x++;
+        $flotilla = $row['flotilla'];
+        $sqlFlotilla= $conn->query("SELECT * FROM flotilla WHERE id = '$flotilla'")->fetch_assoc();
         echo'
         <tr>
             <td>'.$x.'</td>
             <td>'.$row['vehiculo'].'</td>
             <td>'.$row['no_economico'].'</td>
             <td>'.$row['descripcion'].'</td>
+            <td><i class="bi bi-cart-check"></i> '.$sqlFlotilla['flotilla'].'</td>
         </tr>
         ';
     }
