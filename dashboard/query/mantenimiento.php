667fb21e5c4eeacfe7778b7a8e2de6bb8f147b9e
jesusrlv <jesusrlvrojo@gmail.com>
2026-04-20 12:56:42 -0600

    reportes listo 2

--- dashboard/query/mantenimiento.php
+++ dashboard/query/mantenimiento.php
@@ -11,8 +11,7 @@
             <td>'.$x.'</td>
             <td>'.$row['tipo_mantenimiento'].'</td>
                 <td>
-                    <button type="button" class="btn btn-info btn-sm" onclick="reporteMantenimiento('.$row['id'].')"><i class="bi bi-graph-up"></i></button>
-                    <button type="button" class="btn btn-success btn-sm" onclick="modalEditarMantenimiento('.$row['id'].')"><i class="bi bi-file-earmark-post"></i></button>
+                    <button type="button" class="btn btn-success btn-sm" onclick="reporteMantenimiento('.$row['id'].')"><i class="bi bi-file-earmark-post"></i></button>
                     <button type="button" class="btn btn-primary btn-sm" onclick="editarMantenimiento('.$row['id'].')"><i class="bi bi-pencil-square"></i></button>
                     <button type="button" class="btn btn-danger btn-sm" onclick="eliminarMantenimiento('.$row['id'].')"><i class="bi bi-trash3-fill"></i></button>
                 </td>
